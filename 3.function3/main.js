f7dee722de1c7ed8d53db7719107229c9bdfc462
OnceRobert <87625361+OnceRobert@users.noreply.github.com>
2021-07-22 15:58:43 +0800

    Create main.js

---
+++ 3.function3/main.js
@@ -0,0 +1,14 @@
+function alphabetsort(message){
+  x = message.length;
+  for(i=0;i<(x/2);i++){
+   t = message.charAt(i) == message.charAt(x-i-1);
+   document.write(t);
+   if(!t){
+   	return false;
+    }
+    return true;
+  }
+  
+}
+palindrome('hello'); // should return false
+palindrome('abcba'); // should return true
